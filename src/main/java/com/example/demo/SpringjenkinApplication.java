9403d412e61122c527165fc7b9b9e3bb0cf6618c
Pankaj Kumar <kumarp@naomedical.com>
2023-07-14 17:28:12 +0530

    This is first commit

---
+++ src/main/java/com/example/demo/SpringjenkinApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjenkinApplication.class, args);
+		System.out.println("Hello");
+	}
+}
